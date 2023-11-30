c6aee5d47c43f1519f03e5b4e4f7ba43d56f70ab
teki0505 <152475497+teki0505@users.noreply.github.com>
2023-11-30 17:30:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("wss://websocket-p75n.onrender.com/:443");
+let ws = new WebSocket("wss://fish-6z5y.onrender.com");
 
 let controllTD = document.querySelector('.controllTD') ;
 controllTD.addEventListener('input', (event) => {
